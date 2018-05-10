543600e6778ef71990a49720dcc974bdd255aa98
Maria <mynkina@turumburum.com>
2018-05-10 10:53:21 +0300

    second v

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ var result2 = document.getElementById("result2");
 var result3 = document.getElementById("result3");
 var result4 = document.getElementById("result4");
 //1
-for(let i = 0; i < lessThen.length ; i++ ){
-	result1.insertAdjacentHTML('beforebegin','<div>' +  lessThen[i].firstName + " " + lessThen[i].lastName +  " " + lessThen[i].year + '</div>');
-}
+// for(let i = 0; i < lessThen.length ; i++ ){
+// 	result1.insertAdjacentHTML('beforebegin','<div>' +  lessThen[i].firstName + " " + lessThen[i].lastName +  " " + lessThen[i].year + '</div>');
+// }
+lessThen.forEach(function(element) {
+  result1.insertAdjacentHTML('beforebegin','<div>' +  element.firstName + " " + element.lastName +  " " + element.year + '</div>');
+});
 //2
 for(let i = 0; i < names.length ; i++ ){
 	result2.insertAdjacentHTML('afterbegin','<div>' +  names[i] + '</div>');
